36f9e0210c151284fa90869429ee6bc357972cdc
SanJay <sanjaya@qbitum.com>
2022-09-23 14:40:04 +0530

    added radio button

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -7,9 +7,13 @@ class LearnFluttePage extends StatefulWidget {
   State<LearnFluttePage> createState() => _LearnFluttePageState();
 }
 
+enum SingingCharacter { lafayette, jefferson }
+
 class _LearnFluttePageState extends State<LearnFluttePage> {
   bool isSwitch = false;
   bool? isCheckbox = false;
+  bool isRadio = false;
+  SingingCharacter? _character = SingingCharacter.lafayette;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +30,8 @@ class _LearnFluttePageState extends State<LearnFluttePage> {
       ),
       body: Column(
         children: [
-          Switch(
+          Switch.adaptive(
+
               value: isSwitch,
               onChanged: (bool newBool) {
                 setState(() {
@@ -37,7 +42,31 @@ class _LearnFluttePageState extends State<LearnFluttePage> {
             setState(() {
               isCheckbox = newBool;
             });
-          })
+          }),
+          ListTile(
+            title: const Text('Lafayette'),
+            leading: Radio<SingingCharacter>(
+              value: SingingCharacter.lafayette,
+              groupValue: _character,
+              onChanged: (SingingCharacter? value) {
+                setState(() {
+                  _character = value;
+                });
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('Thomas Jefferson'),
+            leading: Radio<SingingCharacter>(
+              value: SingingCharacter.jefferson,
+              groupValue: _character,
+              onChanged: (SingingCharacter? value) {
+                setState(() {
+                  _character = value;
+                });
+              },
+            ),
+          ),
         ],
       ),
     );
